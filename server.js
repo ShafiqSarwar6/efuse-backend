deafba47e4cc917c7d7a5c1fae9898627d509e64
Shafiq Sarwar <shafiqsarwar6@gmail.com>
2021-10-07 22:14:06 +0500

    Dockerizing the node and mongo.

--- server.js
+++ server.js
@@ -3,16 +3,8 @@ const cors = require("cors");
 
 const app = express();
 
-// var corsOptions = {
-//     origin: "http://localhost:8081"
-// };
-
 app.use(cors());
-
-// parse requests of content-type - application/json
 app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 // simple route
